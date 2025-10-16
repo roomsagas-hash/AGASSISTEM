bd202f3c8afef1368f5d1e5b375133490c98b167
roomsagas-hash <roomsagas@gmail.com>
2025-10-16 12:58:02 -0300

    Refactor firebaseConfig.js with placeholder values
    Updated Firebase configuration to use placeholder values for sensitive information.

--- srcfirebaseConfig.js
+++ srcfirebaseConfig.js
@@ -1,26 +1,15 @@
 // firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDCAwnOEtcZ_v583dJWstak6ak5vkBvdks",
-  authDomain: "agas-5ba41.firebaseapp.com",
-  databaseURL: "https://agas-5ba41-default-rtdb.firebaseio.com",
-  projectId: "agas-5ba41",
-  storageBucket: "agas-5ba41.firebasestorage.app",
-  messagingSenderId: "389307205431",
-  appId: "1:389307205431:web:a26f89abc31c428f167cb2",
-  measurementId: "G-PQ8Z6NX3VN"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Inicializa Firestore
-const db = getFirestore(app);
-
-// Exporta o db para ser usado no script principal
-export { db };
+export const db = getFirestore(app);
