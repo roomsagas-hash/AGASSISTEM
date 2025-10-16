900767b8e834f34e507edc977787dd2000a1a364
roomsagas-hash <roomsagas@gmail.com>
2025-10-16 13:05:42 -0300

    Update Firebase configuration and imports

--- srcfirebaseConfig.js
+++ srcfirebaseConfig.js
@@ -1,15 +1,21 @@
-// firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyDCAwnOEtcZ_v583dJWstak6ak5vkBvdks",
+  authDomain: "agas-5ba41.firebaseapp.com",
+  projectId: "agas-5ba41",
+  storageBucket: "agas-5ba41.firebasestorage.app",
+  messagingSenderId: "389307205431",
+  appId: "1:389307205431:web:a26f89abc31c428f167cb2",
+   measurementId: "G-PQ8Z6NX3VN"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
