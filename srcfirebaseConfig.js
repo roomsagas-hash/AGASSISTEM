c764acd5140adb782870f02458af8d75047da5a6
roomsagas-hash <roomsagas@gmail.com>
2025-10-16 12:52:50 -0300

    Refactor Firebase config to use new SDK imports
    Updated Firebase SDK imports and added Firestore initialization.

--- srcfirebaseConfig.js
+++ srcfirebaseConfig.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDCAwnOEtcZ_v583dJWstak6ak5vkBvdks",
   authDomain: "agas-5ba41.firebaseapp.com",
@@ -17,6 +15,12 @@ const firebaseConfig = {
   measurementId: "G-PQ8Z6NX3VN"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Inicializa Firestore
+const db = getFirestore(app);
+
+// Exporta o db para ser usado no script principal
+export { db };
