8b756c96081afe64a0882dfefb61fe2949e89db3
roomsagas-hash <roomsagas@gmail.com>
2025-10-17 20:48:51 -0300

    Update Firebase config with databaseURL
    Added databaseURL to Firebase configuration.

--- srcfirebaseConfig.js
+++ srcfirebaseConfig.js
@@ -9,11 +9,12 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyDCAwnOEtcZ_v583dJWstak6ak5vkBvdks",
   authDomain: "agas-5ba41.firebaseapp.com",
+  databaseURL: "https://agas-5ba41-default-rtdb.firebaseio.com",
   projectId: "agas-5ba41",
   storageBucket: "agas-5ba41.firebasestorage.app",
   messagingSenderId: "389307205431",
   appId: "1:389307205431:web:a26f89abc31c428f167cb2",
-   measurementId: "G-PQ8Z6NX3VN"
+  measurementId: "G-PQ8Z6NX3VN"
 };
 
 // Initialize Firebase
